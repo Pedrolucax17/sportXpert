0106c4c4abdc41f16aa5dc7bcafa9e8a4f06caf4
Pedro Lucas <pl.lucassouza@gmail.com>
2024-09-26 08:48:59 -0300

    Adding Project Dependencies

---
+++ sports/src/main/java/com/sportxpert/sports/SportsxpertApplication.java
@@ -0,0 +1,13 @@
+package com.sportxpert.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsxpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsxpertApplication.class, args);
+	}
+
+}
---
+++ sports/src/test/java/com/sportxpert/sports/SportsxpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportxpert.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsxpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
